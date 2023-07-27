16b4b2fcf176f01fb7cb44308e6582aaceda1b38
kevinshen56714 <kshen64@gatech.edu>
2023-07-27 16:27:33 -0400

    Bump to v2023.8.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extra_files = package_files('pyemc/emc')
 
 setup(
     name='emc-pypi',
-    version='2022.8.1.1',
+    version='2023.8.1',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     description='Python interface for the Enhanced Monte Carlo (EMC) package',
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "Topic :: Software Development",
         "Topic :: Utilities",
